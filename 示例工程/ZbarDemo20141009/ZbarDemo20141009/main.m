fea315c76daee957184cb5f4635aedf6d8a7fd6a
149393437 <zhangcheng@qq.com>
2014-10-14 01:30:30 +0800

    新增
    新增

---
+++ 示例工程/ZbarDemo20141009/ZbarDemo20141009/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZbarDemo20141009
+//
+//  Created by 张诚 on 14-10-9.
+//  Copyright (c) 2014年 zhangcheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZCAppDelegate class]));
+    }
+}
