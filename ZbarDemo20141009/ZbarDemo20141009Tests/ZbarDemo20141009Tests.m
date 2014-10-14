ed70c1c5286f604f8c4b08ca3ea8c307cc1a132f
149393437 <zhangcheng@qq.com>
2014-10-15 01:27:02 +0800

    提交修改
    增加引用

---
+++ ZbarDemo20141009/ZbarDemo20141009Tests/ZbarDemo20141009Tests.m
@@ -0,0 +1,34 @@
+//
+//  ZbarDemo20141009Tests.m
+//  ZbarDemo20141009Tests
+//
+//  Created by 张诚 on 14-10-9.
+//  Copyright (c) 2014年 zhangcheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZbarDemo20141009Tests : XCTestCase
+
+@end
+
+@implementation ZbarDemo20141009Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
